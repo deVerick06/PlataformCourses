1e5d9519f3c081185f6c13babede09cd32ba1608
REI <srickelme658@gmail.com>
2026-02-18 02:40:51 -0300

    Created user model

--- app.py
+++ app.py
@@ -11,3 +11,10 @@ app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(120), unique=True)
+    password = db.Column(db.String(128), nullable=False)
+    
