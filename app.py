8656e1c4dbf8d667d37619bb7a7c4c254c16b3f3
deVerick <elianeheri261206@gmail.com>
2026-02-18 01:17:45 -0300

    Creating the foundation of the project

--- app.py
+++ app.py
@@ -1,2 +1,13 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
