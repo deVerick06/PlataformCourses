a8c3dcf22c542c7c097caa87dd0c822147dac9e6
deVerick06 <elianeheri261206@gmail.com>
2026-03-17 17:35:36 -0300

    Starting styling

--- frontend/src/components/Dashboard/Dashboard.jsx
+++ frontend/src/components/Dashboard/Dashboard.jsx
@@ -1,5 +1,6 @@
 import { useNavigate } from "react-router-dom";
 import { useState, useEffect } from "react";
+import styles from "./Dashboard.module.css";
 
 function Dashboard() {
     const [ myCourses, setMyCourses ] = useState([]);
@@ -32,25 +33,25 @@ function Dashboard() {
     }, []);
 
     return (
-        <>
-            <h2>Meu Dashboard:</h2>
+        <div className={styles.container}>
+            <h2 className={styles.title}>Meu Dashboard:</h2>
             {myCourses.length === 0 ? (
-                <p>Você não se matriculou em nenhum curso ainda</p>
+                <p className={styles.emptyMessage}>Você não se matriculou em nenhum curso ainda</p>
             ) : (
-                <div style={{ display: 'flex', gap: '20px', margin: '10px' }}>
+                <div className={styles.grid}>
                     {myCourses.map((course) => (
-                        <div key={course.id} style={{ border: '1px solid #252525ff', padding: '15px' }}>
-                            <h3>{course.title}</h3>
-                            <span style={{ fontSize: '15px', color: "grey" }}>
+                        <div key={course.id} className={styles.card}>
+                            <h3 className={styles.cardTitle}>{course.title}</h3>
+                            <span className={styles.category}>
                                 {course.category}
                             </span>
                             <br />
-                            <button onClick={() => navigate(`/courses/${course.course_id}`)}>Continuar</button>
+                            <button className={styles.button} onClick={() => navigate(`/courses/${course.course_id}`)}>Continuar</button>
                         </div>
                     ))}
                 </div>
             )}
-        </>
+        </div>
     )
 }
 
